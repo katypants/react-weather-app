930667d0b816e534b708409a82399c4b19a50f6a
Kate Dowd <katewdowd@gmail.com>
2021-08-13 10:16:18 -0400

    Added footer
    With github link

--- src/App.js
+++ src/App.js
@@ -2,7 +2,21 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return <h1 className="App">Weather App</h1>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer className="footer">
+        <a
+          href="https://github.com/katypants/react-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Open-source
+        </a>{" "}
+        coded by Kate Dowd
+      </footer>
+    </div>
+  );
 }
 
 export default App;
