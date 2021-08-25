b983ad872d271f0075915745b517734d6eb51cc1
Kate Dowd <katewdowd@gmail.com>
2021-08-25 14:10:22 -0400

    Created Weather.js, Weather.css
    - search form
    - basic html

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
 import "./App.css";
+import Weather from "./Weather";
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
+        <Weather />
+
         <footer>
           <a
             href="https://github.com/katypants/react-weather-app"
